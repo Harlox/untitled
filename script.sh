348596e521978e12771dc0d72739e9ceb0abd462
Justin <justinmoreau24@gmail.com>
2024-03-21 15:24:47 +0100

    Meme

--- script.sh
+++ script.sh
@@ -1,3 +1,21 @@
 echo "Bonjour, monde !"
 chmod +x script.sh
 read -n 1 -r -s -p "Appuyez sur n'importe quelle touche pour continuer..."
+
+if [ -f script.sh ]; then
+    ./script.sh
+else
+    echo "Le fichier script.sh n'existe pas."
+fi
+
+# Exercice 2
+# Path: script.sh
+echo "Bonjour, monde !"
+chmod +x script.sh
+read -n 1 -r -s -p "Appuyez sur n'importe quelle touche pour continuer..."
+
+if [ -f script.sh ]; then
+    ./script.sh
+else
+    echo "Le fichier script.sh n'existe pas."
+fi
